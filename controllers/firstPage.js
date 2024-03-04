14402af36d9ad05f7599d2eafdd15d8cf82ebb05
parvsehgal <parvsehgal2004@gmail.com>
2024-03-04 19:17:47 +0530

    bug fixes

--- controllers/firstPage.js
+++ controllers/firstPage.js
@@ -2,7 +2,7 @@ const puppeteer = require('puppeteer')
 
 async function getSportsPage(link) {
   const browser = await puppeteer.launch({
-    headless: false,
+    headless: true,
   });
   const page = await browser.newPage();
   await page.goto(link);
