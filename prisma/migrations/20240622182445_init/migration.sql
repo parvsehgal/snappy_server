181976dccec9e19d92430aaa38d2ac4565df79b6
parvsehgal <parvsehgal2004@gmail.com>
2024-06-23 01:33:10 +0530

    setup postgres with prisma

---
+++ prisma/migrations/20240622182445_init/migration.sql
@@ -0,0 +1,22 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Article" (
+    "id" SERIAL NOT NULL,
+    "title" TEXT NOT NULL,
+    "imgUrl" TEXT NOT NULL,
+    "body" TEXT NOT NULL,
+    "userId" INTEGER NOT NULL,
+
+    CONSTRAINT "Article_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Article" ADD CONSTRAINT "Article_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- routes/getData.js
+++ routes/getData.js
@@ -3,12 +3,14 @@ const router = express.Router();
 
 const { sports, politics, entertainment } = require('../controllers/firstPage')
 const { getArticle } = require('../controllers/secondPage')
+const { signUp } = require('../controllers/auth')
 
 router.post('/sports', sports)
 router.post('/politics', politics)
 router.post('/entertainment', entertainment)
 
 router.post('/getArticle', getArticle)
+router.post('/signUp', signUp)
 
 module.exports = router;
 
