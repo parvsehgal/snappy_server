ae27f7cb05a03a234147222650dc2c76db2cb645
parvsehgal <parvsehgal2004@gmail.com>
2024-06-25 21:54:24 +0530

    made controllers for saving and getting daved articles

--- controllers/saveArticle.js
+++ controllers/saveArticle.js
@@ -1,8 +1,32 @@
+const { PrismaClient } = require('@prisma/client');
+const prisma = new PrismaClient();
+
+
 exports.saveArticle = async (req, res) => {
   try {
-    const { name, emai } = req.body
-
+    const { email, article } = req.body
+    const loggedInUser = await prisma.user.findUnique({ where: { email: email } })
+    article.userId = loggedInUser.id;
+    const savedArticle = await prisma.article.create({ data: article })
+    res.status(200).json({ msg: "article saves", savedArticle: savedArticle })
   } catch (err) {
     console.log(err.message)
+    res.status(500).json({ msg: "eoor occured" })
   }
 }
+
+exports.getSavedArticles = async (req, res) => {
+  try {
+    const { email } = req.query;
+    const userWithArticles = prisma.user.findUnique({ where: { email: email }, include: { articles: true } })
+    if (userWithArticles) {
+      res.satus(200).json({ articles: userWithArticles.articles })
+    }
+  } catch (err) {
+    console.log(err.message)
+    res.status(500).json("error occured")
+  }
+}
+
+
+
--- routes/getData.js
+++ routes/getData.js
@@ -4,6 +4,7 @@ const router = express.Router();
 const { sports, politics, entertainment } = require('../controllers/firstPage')
 const { getArticle } = require('../controllers/secondPage')
 const { signUp, login } = require('../controllers/auth')
+const { saveArticle, getSavedArticles } = require('../controllers/saveArticle')
 
 router.post('/sports', sports)
 router.post('/politics', politics)
@@ -12,6 +13,9 @@ router.post('/entertainment', entertainment)
 router.post('/getArticle', getArticle)
 router.post('/signUp', signUp)
 router.post('/login', login)
+
+router.post('/saveArticle', saveArticle)
+router.post('/getSavedArticles', getSavedArticles)
 
 module.exports = router;
 
